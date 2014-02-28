4d999305da4c3678c6616540c6d3f70b46e7da29
Charles Rothbart <chrothbart@gmail.com>
2014-02-28 15:22:39 -0500

    Add pagination

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -98,19 +98,26 @@ describe "User pages" do
 	end
 
 	describe "index" do
-		before do
-			signin FactoryGirl.create(:user)
-			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-			FactoryGirl.create(:user, name: "Eve", email: "eve@example.com")
+		let(:user) { FactoryGirl.create(:user) }
+		before(:each) do
+			signin user
 			visit users_path
 		end
 
 		it { should have_title(full_title('All Users')) }
 		it { should have_content('All users') }
 
-		it "should list each user" do
-			User.all.each do |user|
-				expect(page).to have_selector('li', text: user.name)
+		describe "pagination" do
+			
+			before(:all) 	{ 30.times { FactoryGirl.create(:user) } }
+			after(:all)		{ User.delete_all }
+
+			it { should have_selector('div.pagination') }
+
+			it "should list each user" do
+				User.paginate(page: 1).each do |user|
+					expect(page).to have_selector('li', text: user.name)
+				end
 			end
 		end
 	end
